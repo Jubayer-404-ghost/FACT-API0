dfa631f26e6ee62c402967004470548dc018529a
MR᭄﹅ MAHABUB﹅ メꪜ <mrmahabub345@gmail.com>
2025-05-04 23:52:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get("/video", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2929;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
